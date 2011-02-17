ff6f9a496ac9c548fd26f8f431e6f745a217fa3b
Tom Werth <tom@celect.org>
2011-02-17 17:03:54 -0600

    Added Tom's test spec file.

--- spec/toms_spec.js
+++ spec/toms_spec.js
@@ -1,16 +1,44 @@
-describe("Toms specs", function() {
+describe("jquery.formPaginatorUITools.js", function() {
+
   var custom_form;
 
   beforeEach(function() {
-    loadFixtures('index2.html');
-    // custom_form = $('#test').paginateFormUITools();
+    loadFixtures('form_fixture.html');
+    custom_form = $('.custom_form').paginateFormUITools();
   });
 
-  it('has more than 0 page breaks', function () {
-    // var page_breaks = custom_form.find('div.page_break');
-    // expect(page_breaks.length).toBeGreaterThan( 0 );
-    expect($('.next')).toExist();
+  describe("nextPage()", function() {
+    it('has a "next" button', function () {
+      expect($('.next')).toExist();
+    });
+    it('has a "next" button that is visible', function () {
+      expect($('.next')).toBeVisible();
+    });
+
+
+
+    // it('should call next_page function', function () {
+    //   spyOn(custom_form, 'nextPage');
+    //   expect(custom_form.nextPage).toHaveBeenCalled();
+    // });
+
+
+
+
   });
 
+  describe("previousPage()", function() {
+    it('has a "previous" button', function () {
+      expect($('.previous')).toExist();
+    });
+    it('has a "prev" button that is not visible', function () {
+      expect($('.previous')).not.toBeVisible();
+    });
+  });
 
+  // describe("other stuff", function() {
+  //   it('remove page breaks', function () {
+  //     expect($('div.page_break')).toExist();
+  //   });
+  // });
 });
