a38346da14a5a798e81c3c3f25a2e098c18036dd
Jonathan Mazin <jon@celect.org>
2011-02-22 15:30:34 -0500

    click pass thru

--- spec/paginator_spec.js
+++ spec/paginator_spec.js
@@ -5,6 +5,14 @@ describe("jquery.paginateFormUITools", function() {
 
   // form, one form element, pg break, NOTHING after... next>  what??  next page only has prev?  maybe should strip out EMPTY pages...
 
+  beforeEach(function () {
+    this.addMatchers({
+      toContainText: function(expected) {
+        return this.actual.indexOf( expected ) >= 0;
+      }
+    });
+  });
+
   describe('an empty form', function() {
     it('has no page breaks', function() {
       jasmine.getFixtures().set('<form class="custom_form"/>')
@@ -21,7 +29,6 @@ describe("jquery.paginateFormUITools", function() {
       expect($('.next')).not.toBeVisible();
     });
   });
-
 
   describe('a 3 pagebreak form example', function() {
     beforeEach(function (){
@@ -51,30 +58,47 @@ describe("jquery.paginateFormUITools", function() {
       });
 
       it('has a "Next" button', function () {
-        // console.info('first')
-        // console.info($('input.next'))
-        expect($('input.next')).toBeVisible();
+        expect($('input.next:first')).toBeVisible();
       });
 
       describe('clicking the "Next" link', function() {
         beforeEach(function (){
           var click = $.Event('click');
-          $('input.next').trigger( click );
+          $('input.next:first').trigger( click );
         });
 
         it('has a "Previous" button', function() {
-          // console.info($('input.next'))
-          expect($('input.previous')).toBeVisible();
+          expect($('input.previous:first')).toBeVisible();
         });
 
         it('has a "Next" button', function() {
-          expect($('input.next')).toBeVisible();
+          expect($('input.next:eq(0)')).not.toBeVisible();
+          expect($('input.next:eq(1)')).toBeVisible();
         });
 
         it('does not have a "Submit" button', function() {
           expect($('button.save')).not.toBeVisible();
         });
 
+        describe('clicking the "Next" link again', function() {
+          beforeEach(function (){
+            var click = $.Event('click');
+            $('input.next:eq(1)').trigger( click );
+          });
+
+          it('has a "Previous" button', function() {
+            expect($('input.previous:eq(0)')).not.toBeVisible();
+            expect($('input.previous:eq(1)')).toBeVisible();
+          });
+
+          it('has does not have a "Next" button', function() {
+            expect($('input.next')).not.toBeVisible();
+          });
+
+          it('does have a "Submit" button', function() {
+            expect($('button.save')).toBeVisible();
+          });
+        });
       });
     });
   });
